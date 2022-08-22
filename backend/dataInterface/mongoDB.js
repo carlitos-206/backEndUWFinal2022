e2264d0eb23513dccdc900f5f96e972e73e5f854
Khanh Hoang <khanhqhoang@hotmail.com>
2022-08-22 14:38:54 -0700

    Khanh completed all API checkpoints

--- backend/dataInterface/mongoDB.js
+++ backend/dataInterface/mongoDB.js
@@ -1,33 +1,35 @@
 const { MongoClient } = require("mongodb");
-const ObjectId = require('mongodb').ObjectId;
-const bcrypt = require('bcryptjs');
+const ObjectId = require("mongodb").ObjectId;
+const bcrypt = require("bcryptjs");
 const { use } = require("../routes");
 const uri =
   "mongodb+srv://carlitos206:SharedFakePass123@cluster0.lshmeod.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
-const databaseName = 'washingtonFiresDB';
-const fire_Collection = 'fires'
-const comments_Collection = 'comments';
-const users_Collection = 'users';
+const databaseName = "washingtonFiresDB";
+const fire_Collection = "fires";
+const comments_Collection = "comments";
+const users_Collection = "users";
+const bookmarks_Collection = "bookmarks";
 
-module.exports = {}
-
+module.exports = {};
+// get all fires
 module.exports.getAllFires = async () => {
   try {
     await client.connect();
     const db = client.db(databaseName);
     const collection = db.collection(fire_Collection);
     // Edit limit to get all fires when in prduction
-    const result = await collection.find({}).limit(1).toArray();
+    const result = await collection.find({}).limit(10).toArray();
     return result;
   } catch (err) {
     console.log(err);
   } finally {
     await client.close();
   }
-}
+};
+//get fire by fire id
 module.exports.getFireById = async (id) => {
   try {
     await client.connect();
@@ -40,33 +42,52 @@ module.exports.getFireById = async (id) => {
   } finally {
     await client.close();
   }
-}
+};
+//get fire comment by fire id
 module.exports.getFireComments = async (id) => {
   try {
     await client.connect();
     const db = client.db(databaseName);
     const collection = db.collection(comments_Collection);
-    const result = await collection.find({ fire_id: id }).toArray();
+    const query = { fire_id: ObjectId(id) };
+    const result = await collection.find(query).toArray();
     return result;
   } catch (err) {
     console.log(err);
   } finally {
     await client.close();
   }
-}
+};
+//get fire comment by a commentid
+module.exports.getFireCommentByCommentId = async (commentId) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(comments_Collection);
+    const query = { _id: ObjectId(commentId) };
+    const result = await collection.findOne(query);
+    return result;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+};
+//get all fire comments by userid
 module.exports.getFireCommentsByUser = async (id) => {
   try {
     await client.connect();
     const db = client.db(databaseName);
     const collection = db.collection(comments_Collection);
-    const result = await collection.find({ user_id: id }).toArray();
+    const result = await collection.find({ username: id }).toArray();
     return result;
   } catch (err) {
     console.log(err);
   } finally {
     await client.close();
   }
-}
+};
+//get all fire comment by fire id
 module.exports.getFireCommentsByFire = async (id) => {
   try {
     await client.connect();
@@ -79,8 +100,181 @@ module.exports.getFireCommentsByFire = async (id) => {
   } finally {
     await client.close();
   }
-}
+};
 
+//create a comment by fireid, userid
+module.exports.createComment = async (paramObj, newObj) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(comments_Collection);
+    const commentObj = {
+      ...newObj,
+      fire_id: ObjectId(paramObj.id),
+      username: paramObj.userName,
+      text: newObj.text,
+      createdDate: new Date(),
+    };
+    const result = await collection.insertOne(commentObj);
+    if (result.acknowledged) {
+      return {
+        newObjectId: result.insertedId,
+        message: `Comment created! ID: ${result.insertedId}`,
+      };
+    } else {
+      return { error: "New comment insert failed." };
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+};
+//update comment by comment id
+module.exports.updateComment = async (commentId, newObj) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(comments_Collection);
+    const updateRules = {
+      $set: { text: newObj.text },
+    };
+    const filter = { _id: ObjectId(commentId) };
+    const result = await collection.updateOne(filter, updateRules);
+
+    if (result.modifiedCount != 1) {
+      return {
+        error: `Something went wrong. ${result.modifiedCount} comment was not updated. Please try again.`,
+      };
+    }
+
+    const updatedComment = module.exports.getFireCommentByCommentId(commentId);
+    return updatedComment;
+  } catch (err) {
+    return { Error: "Failed to Update Comment" };
+  } finally {
+    await client.close();
+  }
+};
+//delete comment by comment id
+module.exports.deleteComment = async (commentId) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(comments_Collection);
+
+    const deletionRules = { _id: ObjectId(commentId) };
+    const result = await collection.deleteOne(deletionRules);
+    if (result.deletedCount != 1) {
+      return {
+        error: `Something went wrong. ${result.deletedCount} comments were not deleted. Please try again.`,
+      };
+    }
+
+    return { message: `Deleted ${result.deletedCount} comment.` };
+  } catch (err) {
+    return { Error: "Failed to Delete Comment" };
+  } finally {
+    await client.close();
+  }
+};
+
+//get a bookmark by bookmarkid
+module.exports.getBookmarkByBookmarkId = async(id) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(bookmarks_Collection);
+    const result = await collection.findOne({ _id: ObjectId(id) });
+    return result;
+
+  } catch (err) {
+    return { error: "Failed to retrieve bookmark" };
+  } finally {
+    await client.close();
+  }
+}
+//get all bookmarks by a username
+module.exports.getBookmarkByUserName = async(userName) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(bookmarks_Collection);
+    const result = await collection.find({ username: userName }).toArray();
+    return result;
+
+  } catch (err) {
+    return { error: "Failed to retrieve bookmarks for username: ${userName}" };
+  } finally {
+    await client.close();
+  }
+}
+// get all bookmarks by fireid
+module.exports.getAllBookmarksByFireId = async (id) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(bookmarks_Collection);
+    const result = await collection.find({ fire_id: ObjectId(id) }).toArray();
+    return result;
+
+  } catch (err) {
+    return { error: "Failed to retrieve bookmarks for fire_id: ${id}" };
+  } finally {
+    await client.close();
+  }
+}
+// create a bookmark by fireid and username
+module.exports.createBookmark = async (paramObj) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(bookmarks_Collection);
+    const bookmarkObj = {
+      fire_id: ObjectId(paramObj.id),
+      username: paramObj.userName,
+      createdDate: new Date(),
+    };
+    const result = await collection.insertOne(bookmarkObj);
+    if (result.acknowledged) {
+      return {
+        newObjectId: result.insertedId,
+        message: `Bookmark created! ID: ${result.insertedId}`,
+      };
+    } else {
+      return { error: "New comment insert failed." };
+    }
+
+  }catch (err) {
+    return { error: "Failed to create bookmarks for fire_id: ${id}" };
+  } finally {
+    await client.close();
+  }
+
+}
+//delete a bookmark by bookmarkid
+module.exports.deleteBookmark = async (id) => {
+  try {
+    await client.connect();
+    const db = client.db(databaseName);
+    const collection = db.collection(bookmarks_Collection);
+
+    const deletionRules = { _id: ObjectId(id) };
+    const result = await collection.deleteOne(deletionRules);
+    if (result.deletedCount != 1) {
+      return {
+        error: `Something went wrong. ${result.deletedCount} Bookmarks were not deleted. Please try again.`,
+      };
+    }
+
+    return { message: `Deleted ${result.deletedCount} bookmark.` };
+  } catch (err) {
+    return { Error: "Failed to Delete a bookmark" };
+  } finally {
+    await client.close();
+  }
+};
+//create or register a user
 
 module.exports.createUser = async (user) => {
   try {
@@ -90,56 +284,63 @@ module.exports.createUser = async (user) => {
     let emailExist = await collection.findOne({ email: user.email });
     if (emailExist) {
       return { Error: "Email already exists" };
-    }
-    else{
+    } else {
       let hashedPassword = await bcrypt.hash(user.password, 10);
       let vettedUser = {
         username: user.username,
         email: user.email,
         password: hashedPassword,
         firstName: user.firstName,
-        lastName: user.lastName
+        lastName: user.lastName,
       };
       const result = await collection.insertOne(vettedUser);
-      return { newObjectId: result.insertedId, message: `User created! ID: ${result.insertedId}`, pass: hashedPassword };
+      return {
+        newObjectId: result.insertedId,
+        message: `User created! ID: ${result.insertedId}`,
+        pass: hashedPassword,
+      };
     }
   } catch (err) {
-    return {Error: 'Failed to Create'};
+    return { Error: "Failed to Create User" };
   } finally {
     await client.close();
   }
-}
+};
 
-// 
-module.exports.signIn = async(user) =>{
-  try{
+//user login
+module.exports.signIn = async (user) => {
+  try {
     await client.connect();
     const db = client.db(databaseName);
     const collection = db.collection(users_Collection);
-    const userExistEmail = await collection.findOne({email: user.usernameOrEmail})
-    const userExistUsername = await collection.findOne({username: user.usernameOrEmail})
-    if(userExistEmail){
-      if(await bcrypt.compare(user.password, userExistEmail.password )){
-        return {username: userExistEmail.username, email: userExistEmail.email}
+    const userExistEmail = await collection.findOne({
+      email: user.usernameOrEmail,
+    });
+    const userExistUsername = await collection.findOne({
+      username: user.usernameOrEmail,
+    });
+    if (userExistEmail) {
+      if (await bcrypt.compare(user.password, userExistEmail.password)) {
+        return {
+          username: userExistEmail.username,
+          email: userExistEmail.email,
+        };
       }
     }
-    if(userExistUsername){
-      if(await bcrypt.compare(user.password, userExistUsername.password)){
-        return {username: userExistUsername.username, email: userExistUsername.email}
+    if (userExistUsername) {
+      if (await bcrypt.compare(user.password, userExistUsername.password)) {
+        return {
+          username: userExistUsername.username,
+          email: userExistUsername.email,
+        };
       }
     }
   } catch (err) {
-    return {error: 'Failed to locate user'};
+    return { error: "Failed to locate user" };
   } finally {
     await client.close();
   }
-}
-
-
-
-
-
-
+};
 
 // IGNORE BELOW THIS LINE BUT KEEP FOR REFERENCE
 
@@ -158,38 +359,38 @@ module.exports.signIn = async(user) =>{
 // }
 // ......
 // example:
-// { 
-//   "properties": { 
-//     "CalculatedAcres": 50.64, 
+// {
+//   "properties": {
+//     "CalculatedAcres": 50.64,
 //     "ContainmentDateTime": "2020-08-06T23:13:07Z",
-//     "ControlDateTime": "2020-08-06T23:13:24Z", 
-//     "DailyAcres": 50.6, 
-//     "DiscoveryAcres": 20.0, 
-//     "DispatchCenterID": "MTMCC", 
-//     "EstimatedCostToDate": null, 
-//     "FinalFireReportApprovedByTitle": null, 
-//     "FinalFireReportApprovedDate": "2020-08-12T20:45:59Z", 
-//     "FireCause": "Human", 
-//     "FireCauseGeneral": "Equipment", 
-//     "FireCauseSpecific": "Farming Equipment", 
-//     "FireDiscoveryDateTime": "2020-08-06T18:58:00Z", 
-//     "FireOutDateTime": "2020-08-12T14:00:00Z", 
-//     "IncidentName": "Ramme", 
-//     "InitialLatitude": 45.78496, 
-//     "InitialLongitude": -104.4958, 
-//     "POOCity": null, 
-//     "POOCounty": "Carter", 
-//     "POOState": "US-MT", 
-//     "PredominantFuelGroup": "Grass", 
-//     "ModifiedBySystem": "INFORM_Inspector", 
-//     "CreatedOnDateTime_dt": "2020-08-06T19:50:29Z", 
-//     "ModifiedOnDateTime_dt": "2020-08-12T20:46:01Z", 
-//     "Source": "IRWIN", 
-//     }, 
-//     "geometry": { 
-//       "type": "Point", 
-//       "coordinates": [ -104.457510856999988, 45.785037225000053 ] 
-//       } 
+//     "ControlDateTime": "2020-08-06T23:13:24Z",
+//     "DailyAcres": 50.6,
+//     "DiscoveryAcres": 20.0,
+//     "DispatchCenterID": "MTMCC",
+//     "EstimatedCostToDate": null,
+//     "FinalFireReportApprovedByTitle": null,
+//     "FinalFireReportApprovedDate": "2020-08-12T20:45:59Z",
+//     "FireCause": "Human",
+//     "FireCauseGeneral": "Equipment",
+//     "FireCauseSpecific": "Farming Equipment",
+//     "FireDiscoveryDateTime": "2020-08-06T18:58:00Z",
+//     "FireOutDateTime": "2020-08-12T14:00:00Z",
+//     "IncidentName": "Ramme",
+//     "InitialLatitude": 45.78496,
+//     "InitialLongitude": -104.4958,
+//     "POOCity": null,
+//     "POOCounty": "Carter",
+//     "POOState": "US-MT",
+//     "PredominantFuelGroup": "Grass",
+//     "ModifiedBySystem": "INFORM_Inspector",
+//     "CreatedOnDateTime_dt": "2020-08-06T19:50:29Z",
+//     "ModifiedOnDateTime_dt": "2020-08-12T20:46:01Z",
+//     "Source": "IRWIN",
+//     },
+//     "geometry": {
+//       "type": "Point",
+//       "coordinates": [ -104.457510856999988, 45.785037225000053 ]
+//       }
 // },
 
 // module.exports.create = async (data, setOrigin) => {
