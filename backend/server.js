6ad9baad4292a4ac7f299ef8b5a8336bb00fe775
carlitos-206 <carloscaceres041@gmail,com>
2022-09-02 06:30:37 -0700

    cors update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+// cor updated to match the netlify url
 const corsOptions = {origin:"https://famous-tanuki-4913b3.netlify.app/"};
 
 const server = express();
