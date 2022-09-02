7dcfd0ed79c6642194a9c6759a3a5b450dd3707e
carlitos-206 <carloscaceres041@gmail,com>
2022-09-02 06:07:31 -0700

    updated cors fix test

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+const corsOptions = {origin:"https://famous-tanuki-4913b3.netlify.app/"};
 
 const server = express();
 server.use(express.json());
-server.use(cors());
-const corsOptions = {origin:"http://localhost:3000"};
+server.use(cors(corsOptions));
 
 server.use(routes);
 
