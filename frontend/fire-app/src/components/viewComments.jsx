41bcba6ded3ac6dd7181c8a29937d70b6ecd985b
carlitos-206 <carloscaceres041@gmail,com>
2022-09-05 06:37:44 -0700

    eod

--- frontend/fire-app/src/components/viewComments.jsx
+++ frontend/fire-app/src/components/viewComments.jsx
@@ -3,11 +3,12 @@ import readTheDate from './logic/readTheDate';
 const axios = require('axios')
 export default function ViewComments({fire_id}){
   const [comments, setData] = useState(null)  
-    const refresh = () =>{
-      axios.get(`https://uw-api-2022.herokuapp.com/fires/${fire_id}/comments`)
-      .then(response =>
-        setData(response.data)
-      )
+  
+  const refresh = () =>{
+    axios.get(`https://uw-api-2022.herokuapp.com/fires/${fire_id}/comments`)
+    .then(response =>
+      setData(response.data)
+    )
     }
   setTimeout(()=>{
     refresh()
@@ -24,37 +25,36 @@ export default function ViewComments({fire_id}){
       setData(response.data)
     )
 },[])
-  const local = localStorage.getItem('loginData')
-  const localObj = JSON.parse(local)
-  if(comments !== null){
-    let reverseList = comments.reverse()
-    return (
-      <div className="fireCommentSection">
-        {reverseList.map((data, idx)=>{
-          if(localObj){    
-            if(localObj.username === data.username){
-              const userBtns = document.getElementsByClassName('userOwnCommentBtn')
-              for(let i = 0; i < userBtns.length; i++){
-
-                userBtns[i].setAttribute("style", 'display:block')
-              }
-            }
+const local = localStorage.getItem('loginData')
+const localObj = JSON.parse(local)
+if(comments !== null){
+  let reverseList = comments.reverse()
+  return (
+    <div className="fireCommentSection">
+      {reverseList.map((data, idx)=>{
+      if(localObj){    
+        if(localObj.username === data.username){
+          const userBtns = document.getElementsByClassName('userOwnCommentBtn')
+          for(let i = 0; i < userBtns.length; i++){
+            userBtns[i].setAttribute("style", 'display:block')
           }
-            return(
-              <div className="fireComments" key={idx}>
-                <div className="commentBody">
-                  {data.text}
-                </div>
-                <div className="commentAuthor">
-                  Posted By: {data.username} ON {readTheDate(data.createdDate)}
-                </div>
-                <div>
-                  <button className="userOwnCommentBtn" style={{"display":"none"}}>Edit</button>
-                  <button className="userOwnCommentBtn" style={{"display":"none"}}>Delete</button>
-                </div>
-            </div>
-            )}
-          )}
+        }
+      }
+      return(
+        <div className="fireComments" key={idx}>
+          <div className="commentBody">
+            {data.text}
+          </div>
+          <div className="commentAuthor">
+            Posted By: {data.username} ON {readTheDate(data.createdDate)}
+          </div>
+          <div>
+            <button className="userOwnCommentBtn" style={{"display":"none"}}>Edit</button>
+            <button className="userOwnCommentBtn" style={{"display":"none"}}>Delete</button>
+          </div>
+        </div>
+        )}
+        )}
       </div>
     )
   }
